3835299786edf81f05b9fa7f9ad50d373569d3c5
david-bush <David.C.Bush@outlook.com>
2018-10-20 14:40:12 -0500

    Adding database and API

---
+++ API/src/main/java/com/hacktx/api/HackTXApplication.java
@@ -0,0 +1,12 @@
+package com.hacktx.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackTXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackTXApplication.class, args);
+	}
+}
